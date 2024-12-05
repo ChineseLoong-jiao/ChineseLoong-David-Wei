7c4715a8d800bddea04f49cc9822d0e200aa22ab
魏文皓 <3590913187@qq.com>
2024-12-06 00:43:10 +0800

    Add files via upload

---
+++ iterative_integer_arithmetic_for_pi_val.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+long b, c = 2800, d, e, f[2801], g;
+void main(void)
+{
+    for (; b - c;)
+        f[b++] = 10000 / 5;
+    for (; d = 0, g = c * 2; c = 14, printf("%.4d", e + d / 10000), e = d % 10000)
+        for (b = c; d += f[b] * 10000, f[b] = d % --g, d /= g--, --b; d *= b)
+            ;
+    getch();
+}
